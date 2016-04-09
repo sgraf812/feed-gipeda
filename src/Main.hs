7b96a1171bd9c881e9f0a0cd78b0f96a2e6ae249
Sebastian Graf <sgraf1337@gmail.com>
2016-04-09 14:33:00 +0200

    Well, it's still somewhat crude, but distribution works. Fixes #2

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,23 @@
-import Control.Concurrent.Chan (Chan, newChan, writeChan, readChan)
 import qualified Config
 import           Control.Concurrent       (forkIO)
-import           Control.Monad            (forever, when)
+import           Control.Concurrent.Chan  (Chan, newChan, readChan, writeChan)
+import           Control.Monad            (forever, when, void)
 import           Data.Set                 (Set)
 import           Data.Time                (NominalDiffTime)
+import qualified Distributed
 import           GitShell                 (SHA)
 import           Network.URI              (parseURI)
 import           Repo                     (Repo)
 import qualified RepoWatcher
-import           System.Console.ArgParser (Descr (..), ParserSpec, andBy,
-                                           boolFlag, optFlag, parsedBy,
+import           System.Console.ArgParser (Descr (..), ParserSpec,
+                                           andBy, boolFlag,
+                                           optFlag, optFlagArgs, parsedBy,
                                            withParseResult)
 import           System.Directory         (getAppUserDataDirectory)
 import           System.Exit              (exitSuccess)
 import           System.FilePath          ((</>))
+import           System.ZMQ4.Monadic      (ZMQ, async, liftIO)
 import qualified Worker
-import qualified Distributed
-import System.ZMQ4.Monadic (ZMQ, async, liftIO)
 
 
 data CmdArgs
@@ -26,8 +27,14 @@ data CmdArgs
   , configFile      :: FilePath
   , fetchInterval   :: Int
   , check           :: Bool
-  , rsyncPath       :: String
+  , rsyncPath       :: Maybe String
+  , brokerAt        :: Maybe Int
+  , workFor         :: Maybe String
   }
+
+
+maybeFlag key =
+  optFlagArgs Nothing key Nothing (const Just)
 
 
 cmdParser :: FilePath -> ParserSpec CmdArgs
@@ -43,40 +50,47 @@ cmdParser configFile = CmdArgs
   `andBy` optFlag (60*60) "dt" `Descr` "Fetch interval for all repos in seconds."
   ++ " Defaults to 60*60 ~= 1 hour"
   `andBy` boolFlag "check" `Descr` "Verify that the given config file is well-formed and exit"
-  `andBy` optFlag "" "rsync" `Descr` "ssh path under which to deploy site/ folders"
+  `andBy` maybeFlag "rsync" `Descr` "ssh path under which to deploy site/ folders"
   ++ " with rsync"
+  `andBy` maybeFlag "broker-at" `Descr` "Start in master mode, distributing work items at"
+  ++ " the given TCP port number."
+  `andBy` maybeFlag "work-for" `Descr` "Start in slave mode, requesting work items from"
+  ++ " the given TCP endpoint."
 
 
 main :: IO ()
 main = do
   configFile <- getAppUserDataDirectory ("feed-gipeda" </> "feed-gipeda.yaml")
-  withParseResult (cmdParser configFile) $ \(CmdArgs cloben gipeda configFile dt check rsyncPath) -> do
+  withParseResult (cmdParser configFile) $
+    \(CmdArgs cloben gipeda configFile dt check rsyncPath brokerAt workFor) -> do
     -- Handle the --check flag. Just perform a syntax check on the given configFile
     when check $ Config.checkFile configFile >>= maybe exitSuccess fail
 
-    let endpoint = "ipc://feed-gipeda.ipc"
+    -- translate the config flags to an endpoint
+    (endpoint, broker, work) <- case (brokerAt, workFor) of
+      (Nothing, Nothing) -> return ("ipc://feed-gipeda.ipc", True, True)
+      (Just port, Nothing) -> return ("tcp://*:" ++ show port, True, False)
+      (Nothing, Just endpoint) -> return ("tcp://" ++ endpoint, False, True)
+      _ -> fail "Cannot work in both broker and worker mode. Launch as two different processes instead."
 
-    Distributed.withBroker endpoint (Worker.finalize gipeda rsyncPath) $ \send -> do
+    if not broker
+      then Distributed.worker endpoint (Worker.benchmark cloben)
+      else Distributed.withBroker endpoint (Worker.finalize gipeda rsyncPath) $ \send -> do
+        -- Performs the benchmarking and site generation by calling the appropriate
+        -- scripts.
+        -- I'm unsure about whether this should be parallelized with multiple workers.
+        -- Performance couldn't be compared among builds anymore.
+        (when work . void . forkIO . Distributed.worker endpoint) (Worker.benchmark cloben)
+        -- Each time the config file @f@ changes, we @cloneAddedRepos@ (New repos with
+        -- new SHAs).
+        -- We also touch the config file initially (recognizing local clones and
+        -- already benchmarked commits) and check all clones for updated remotes
+        -- (old Repos, new SHAs).
+        -- New @(Repo, SHA)@ pairs notify the @onNewCommits@ handler, which will
+        -- push @WorkItem@s along the @workItems@ queue to the worker.
+        let
+          onNewCommits :: Repo -> Set SHA -> IO ()
+          onNewCommits repo =
+            mapM_ (send repo)
 
-      let
-        onNewCommits :: Repo -> Set SHA -> IO ()
-        onNewCommits repo =
-          mapM_ (send repo)
-
-      -- We have to run the benchmark worker on the main thread so that asynchronous
-      -- @UserInterrupt@s are handled correspondingly (e.g. by deleting the touched
-      -- .log file).
-
-      -- Each time the config file @f@ changes, we @cloneAddedRepos@ (New repos with
-      -- new SHAs).
-      -- We also touch the config file initially (recognizing local clones and
-      -- already benchmarked commits) and check all clones for updated remotes
-      -- (old Repos, new SHAs).
-      -- New @(Repo, SHA)@ pairs notify the @onNewCommits@ handler, which will
-      -- push @WorkItem@s along the @workItems@ queue to the worker.
-      forkIO (RepoWatcher.watchConfiguredRepos configFile (fromIntegral dt) onNewCommits)
-      -- Performs the benchmarking and site generation by calling the appropriate
-      -- scripts.
-      -- I'm unsure about whether this should be parallelized with multiple workers.
-      -- Performance couldn't be compared among builds anymore.
-      Distributed.worker endpoint (Worker.benchmark gipeda cloben rsyncPath)
+        RepoWatcher.watchConfiguredRepos configFile (fromIntegral dt) onNewCommits
