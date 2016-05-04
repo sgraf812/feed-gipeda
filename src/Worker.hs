9c8e359ccf77da53fb0611f6f1a9722d71665831
Sebastian Graf <sgraf1337@gmail.com>
2016-05-04 15:34:07 +0200

    Interplay with the benchmark script is now as intended

--- src/Worker.hs
+++ src/Worker.hs
@@ -2,9 +2,7 @@ module Worker
   ( benchmark
   ) where
 
-{-| The 'meat' of the daemon. @work@ calls the @--benchmark@ script for
-    new @Benchmark@ items resp. the @--gipeda@ script for new @Regenerate@
-    items.
+{-| @benchmark@ contains the logic to be executed on slave nodes.
 -}
 
 import           Control.Logging as Logging
@@ -18,43 +16,52 @@ import           System.Process  (cwd, proc, readCreateProcessWithExitCode,
                                   shell, showCommandForUser)
 
 
-procReportingError :: Maybe FilePath -> FilePath -> [String] -> IO String
-procReportingError cwd cmd args = do
+procReportingError :: Repo -> SHA -> Maybe FilePath -> FilePath -> [String] -> IO String
+procReportingError repo commit cwd cmd args = do
   (exitCode, stdout, stderr) <-
     readCreateProcessWithExitCode (proc cmd args) { cwd = cwd } ""
-  reportError (showCommandForUser cmd args) exitCode stderr
+  reportError repo commit (showCommandForUser cmd args) exitCode stderr
   return stdout
 
 
-shellReportingError :: Maybe FilePath -> FilePath -> IO String
-shellReportingError cwd cmd = do
+shellReportingError :: Repo -> SHA -> Maybe FilePath -> FilePath -> IO String
+shellReportingError repo commit cwd cmd = do
   (exitCode, stdout, stderr) <-
     readCreateProcessWithExitCode (shell cmd) { cwd = cwd } ""
-  reportError cmd exitCode stderr
+  reportError repo commit cmd exitCode stderr
   return stdout
 
 
-reportError :: String -> ExitCode -> String -> IO ()
-reportError cmd code stderr =
+reportError :: Repo -> SHA -> String -> ExitCode -> String -> IO ()
+reportError repo commit cmd code stderr =
   case code of
     ExitSuccess -> return ()
-    ExitFailure c -> do
-      Logging.warn (Text.pack (cmd ++ ": exit code " ++ show c))
-      Logging.warn (Text.pack stderr)
+    ExitFailure c ->
+      Logging.warn . Text.pack . unlines $
+        [ "Benchmark script error"
+        , "At commit " ++ Repo.uri repo ++ "@" ++ commit ++ ":"
+        , cmd ++ ": exit code " ++ show c
+        , stderr
+        ]
 
 
 cloneRecursiveAndCheckout :: Repo -> SHA -> FilePath -> IO ()
 cloneRecursiveAndCheckout repo commit cloneDir = do
-  procReportingError Nothing "git" ["clone", "--quiet", Repo.uri repo, cloneDir]
-  procReportingError (Just cloneDir) "git" ["reset", "--hard", commit]
-  shellReportingError (Just cloneDir) "git submodule update --init --recursive --quiet"
+  procReportingError repo commit Nothing "git" ["clone", "--quiet", Repo.uri repo, cloneDir]
+  procReportingError repo commit (Just cloneDir) "git" ["reset", "--hard", commit]
+  shellReportingError repo commit (Just cloneDir) "git submodule update --init --recursive --quiet"
   return ()
 
 
+{-| Clones the given @repo@ at a specific @commit@ into a temporary directory.
+    Then calls the benchmark script within that directory and returns its output.
+
+    Will be executed on slave nodes.
+-}
 benchmark :: FilePath -> Repo -> SHA -> IO String
 benchmark benchmarkScript repo commit = do
   clone <- Repo.cloneDir repo
   Logging.log (Text.pack ("Benchmarking " ++ Repo.uri repo ++ "@" ++ commit))
   withSystemTempDirectory "feed-gipeda" $ \cloneDir -> do
     cloneRecursiveAndCheckout repo commit cloneDir
-    shellReportingError (Just cloneDir) benchmarkScript
+    shellReportingError repo commit (Just cloneDir) benchmarkScript
