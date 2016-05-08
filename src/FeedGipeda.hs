a6392eba82ec3ee95db6e2f3394ae7de61c5d46f
Sebastian Graf <sgraf1337@gmail.com>
2016-05-08 15:11:01 +0200

    Split Main into two modules and switched to optparse-applicative

---
+++ src/FeedGipeda.hs
@@ -0,0 +1,117 @@
+module FeedGipeda
+  ( Endpoint (..)
+  , feedGipeda
+  ) where
+
+
+import qualified Config
+import           Control.Arrow                                      (second)
+import           Control.Concurrent                                 (forkIO)
+import           Control.Concurrent.Chan                            (Chan,
+                                                                     newChan,
+                                                                     readChan,
+                                                                     writeChan)
+import           Control.Distributed.Process                        (Process,
+                                                                     RemoteTable,
+                                                                     liftIO,
+                                                                     say,
+                                                                     spawnLocal)
+import qualified Control.Distributed.Process.Backend.SimpleLocalnet as SLN
+import           Control.Distributed.Process.Node                   (initRemoteTable,
+                                                                     runProcess)
+import           Control.Logging                                    as Logging
+import           Control.Monad                                      (forever,
+                                                                     void, when)
+import           Data.List                                          (elemIndex)
+import           Data.Maybe                                         (isJust)
+import           Data.Set                                           (Set)
+import           Data.Time                                          (NominalDiffTime)
+import           GitShell                                           (SHA)
+import qualified Master
+import           Network.URI                                        (parseURI)
+import           Repo                                               (Repo)
+import           System.Directory                                   (getAppUserDataDirectory)
+import           System.Exit                                        (exitSuccess)
+import           System.FilePath                                    ((</>))
+import qualified TaskQueue
+import qualified THGenerated
+import qualified Worker
+
+
+data Endpoint
+  = Endpoint
+  { host :: String
+  , port :: Int
+  } deriving (Show, Eq)
+
+
+remoteTable :: RemoteTable
+remoteTable =
+  THGenerated.__remoteTable initRemoteTable
+
+
+feedGipeda
+  :: FilePath -> FilePath
+  -> Bool -> Int -> Bool
+  -> Maybe String
+  -> Maybe Endpoint -> Maybe Endpoint
+  -> Bool
+  -> IO ()
+feedGipeda
+  gipeda configFile
+  oneShot dt check
+  rsyncPath
+  master slave
+  verbose = do
+  if verbose
+    then Logging.setLogLevel Logging.LevelDebug
+    else Logging.setLogLevel Logging.LevelWarn
+
+  -- Handle the --check flag. Just perform a syntax check on the given configFile
+  when check $ Config.checkFile configFile >>= maybe exitSuccess fail
+
+  -- translate the config flags to endpoints
+  let
+    (masterEndpoint, workerEndpoint) =
+      case (master, slave) of
+        (Nothing, Nothing) -> (Just (Endpoint "localhost" 1337), Just (Endpoint "localhost" 1338))
+        _ -> (master, slave)
+
+  case workerEndpoint of
+    Just (Endpoint host port) -> do
+      let
+        run = if isJust masterEndpoint then void . forkIO else id
+      run $ do
+        backend <- SLN.initializeBackend host (show port) remoteTable
+        TaskQueue.work backend
+    _ -> return ()
+
+  case masterEndpoint of
+    Nothing -> return ()
+    Just (Endpoint host port) -> do
+      backend <- SLN.initializeBackend host (show port) remoteTable
+      node <- SLN.newLocalNode backend
+      tasks <- newChan
+      runProcess node $ do
+        taskQueue <- TaskQueue.start backend
+
+        spawnLocal $ forever $ do
+          (finalize, benchmarkScript, repo, commit) <- liftIO (readChan tasks)
+          spawnLocal $ do
+            result <- TaskQueue.execute taskQueue
+              THGenerated.stringDict
+              (THGenerated.benchmarkClosure benchmarkScript repo commit)
+            liftIO (finalize result)
+
+        let
+          onNewCommit :: (String -> IO ()) -> String -> Repo -> SHA -> IO ()
+          onNewCommit finalize benchmarkScript repo commit =
+            writeChan tasks (finalize, benchmarkScript, repo, commit)
+
+          paths :: Master.Paths
+          paths =
+            Master.Paths configFile rsyncPath gipeda
+
+        liftIO $ if oneShot
+          then Master.checkForNewCommits paths Master.OneShot onNewCommit
+          else Master.checkForNewCommits paths (Master.PeriodicRefresh (fromIntegral dt)) onNewCommit
--- src/Main.hs
+++ src/Main.hs
@@ -1,155 +1,72 @@
-import qualified Config
-import           Control.Arrow                                      (second)
-import           Control.Concurrent                                 (forkIO)
-import           Control.Concurrent.Chan                            (Chan,
-                                                                     newChan,
-                                                                     readChan,
-                                                                     writeChan)
-import           Control.Distributed.Process                        (Process,
-                                                                     RemoteTable,
-                                                                     liftIO,
-                                                                     say,
-                                                                     spawnLocal)
-import qualified Control.Distributed.Process.Backend.SimpleLocalnet as SLN
-import           Control.Distributed.Process.Node                   (initRemoteTable,
-                                                                     runProcess)
-import           Control.Logging                                    as Logging
-import           Control.Monad                                      (forever,
-                                                                     void, when)
-import           Data.List                                          (elemIndex)
-import           Data.Maybe                                         (isJust)
-import           Data.Set                                           (Set)
-import           Data.Time                                          (NominalDiffTime)
-import           GitShell                                           (SHA)
-import qualified Master
-import           Network.URI                                        (parseURI)
-import           Repo                                               (Repo)
-import           System.Console.ArgParser                           (Descr (..),
-                                                                     ParserSpec,
-                                                                     andBy,
-                                                                     boolFlag,
-                                                                     optFlag,
-                                                                     optFlagArgs,
-                                                                     parsedBy,
-                                                                     withParseResult)
-import           System.Directory                                   (getAppUserDataDirectory)
-import           System.Exit                                        (exitSuccess)
-import           System.FilePath                                    ((</>))
-import qualified TaskQueue
-import qualified THGenerated
-import qualified Worker
+import           Control.Logging     as Logging
+import           Control.Monad       (join)
+import           Data.List           (elemIndex)
+import qualified FeedGipeda
+import           Options.Applicative
+import           System.Directory    (getAppUserDataDirectory)
+import           System.FilePath     ((</>))
+import           Text.Read           (readMaybe)
 
 
-data CmdArgs
-  = CmdArgs
-  { gipeda        :: FilePath
-  , configFile    :: Maybe FilePath
-  , oneShot       :: Bool
-  , fetchInterval :: Int
-  , check         :: Bool
-  , rsyncPath     :: Maybe String
-  , master        :: Maybe String
-  , slave         :: Maybe String
-  , verbose       :: Bool
-  }
-
-
-maybeFlag key =
-  optFlagArgs Nothing key Nothing (const Just)
-
-
-cmdParser :: ParserSpec CmdArgs
-cmdParser = CmdArgs
-  `parsedBy` optFlag "gipeda" "gipeda" `Descr` "Path to the gipeda executable"
-  ++ " the directory of which includes assets such as site/ scaffolding and install-jslibs.sh"
-  `andBy` maybeFlag "config" `Descr` "Path to the YAML file containing"
-  ++ " a list of watched repositories. Will be watched for changes."
-  ++ " Defaults to the .feed-gipeda/feed-gipeda.yaml sub path under"
-  ++ " $HOME resp. %APPDATA%/Roaming/"
-  `andBy` boolFlag "one-shot" `Descr` "Fetch updated repositories only once and"
-  ++ " exit after all new commits have been handled."
-  `andBy` optFlag (60*60) "dt" `Descr` "Fetch interval for all repos in seconds."
-  ++ " Defaults to 60*60 ~= 1 hour"
-  `andBy` boolFlag "check" `Descr` "Verify that the given config file is well-formed and exit"
-  `andBy` maybeFlag "rsync" `Descr` "ssh path under which to deploy site/ folders"
-  ++ " with rsync"
-  `andBy` maybeFlag "master" `Descr` "Start in master mode, distributing work items."
-  ++ " Identified via the given TCP endpoint (ipadress:portnumber)."
-  `andBy` maybeFlag "slave" `Descr` "Start in slave mode, requesting work items from a master node."
-  ++ " Identified via the given TCP endpoint (ipadress:portnumber)."
-  `andBy` boolFlag "verbose" `Descr` "Show log messages intended for debugging"
-
-
-parseEndpoint :: String -> IO (String, String)
-parseEndpoint s =
+endpoint :: ReadM FeedGipeda.Endpoint
+endpoint = do
+  s <- str
   case elemIndex ':' s of
-    Just idx -> return (second tail (splitAt idx s))
-    Nothing -> fail ("Could not parse endpoint " ++ s)
+    Nothing -> readerError "Expected a colon separator"
+    Just idx -> do
+      let (host, port') = splitAt idx s
+      case readMaybe port' of
+        Just port -> return (FeedGipeda.Endpoint host port)
+        Nothing -> readerError "Port was not integral"
 
 
-remoteTable :: RemoteTable
-remoteTable =
-  THGenerated.__remoteTable initRemoteTable
+parser :: IO (Parser (IO ()))
+parser = do
+  defaultConfig <- getAppUserDataDirectory ("feed-gipeda" </> "feed-gipeda.yaml")
+  return $
+    FeedGipeda.feedGipeda
+      <$> option str
+            (long "gipeda"
+              <> value "gipeda"
+              <> metavar "FILEPATH"
+              <> help "Path to the gipeda executable")
+      <*> option str
+            (long "config"
+              <> value defaultConfig
+              <> metavar "FILEPATH"
+              <> help ("Path to the YAML file containing a list of watched repositories. Will be watched for changes. Defaults to " ++ defaultConfig ++ "."))
+      <*> switch
+            (long "one-shot"
+              <> help "Fetch updated repositories only once and exit after all new commits have been handled.")
+      <*> option auto
+            (long "dt"
+              <> metavar "SECONDS"
+              <> value (60*60)
+              <> help "Fetch interval for all repos in seconds. Default to one hour.")
+      <*> switch
+            (long "check"
+              <> help "Verify that the given config file is well-formed and exit")
+      <*> option (optional str)
+            (long "rsync"
+              <> metavar "SSH_PATH"
+              <> help "ssh path under which to deploy site/ folders with rsync")
+      <*> option (optional endpoint)
+            (long "master"
+              <> metavar "ENDPOINT"
+              <> help "Start in master mode, distributing work items. Identified via the given TCP endpoint (ipadress:portnumber).")
+      <*> option (optional endpoint)
+            (long "slave"
+              <> metavar "ENDPOINT"
+              <> help "Start in slave mode, requesting work items from a master node. Identified via the given TCP endpoint (ipadress:portnumber).")
+      <*> switch
+            (long "verbose"
+              <> help "Show log messages intended for debugging")
 
 
 main :: IO ()
-main = Logging.withStderrLogging $ withParseResult cmdParser $
-  \(CmdArgs gipeda configFile' oneShot dt check rsyncPath master slave verbose) -> do
-    if verbose
-      then Logging.setLogLevel Logging.LevelDebug
-      else Logging.setLogLevel Logging.LevelWarn
-
-    configFile <- maybe
-      (getAppUserDataDirectory ("feed-gipeda" </> "feed-gipeda.yaml"))
-      return
-      configFile'
-
-    -- Handle the --check flag. Just perform a syntax check on the given configFile
-    when check $ Config.checkFile configFile >>= maybe exitSuccess fail
-
-    master' <- maybe (return Nothing) (fmap Just . parseEndpoint) master
-    slave' <- maybe (return Nothing) (fmap Just . parseEndpoint) slave
-
-    -- translate the config flags to endpoints
-    (masterEndpoint, workerEndpoint) <- case (master', slave') of
-      (Nothing, Nothing) -> return (Just ("localhost", "1337"), Just ("localhost", "1338"))
-      _ -> return (master', slave')
-
-    case workerEndpoint of
-      Just (host, port) -> do
-        let
-          run = if isJust masterEndpoint then void . forkIO else id
-        run $ do
-          backend <- SLN.initializeBackend host port remoteTable
-          TaskQueue.work backend
-      _ -> return ()
-
-    case masterEndpoint of
-      Nothing -> return ()
-      Just (host, port) -> do
-        backend <- SLN.initializeBackend host port remoteTable
-        node <- SLN.newLocalNode backend
-        tasks <- newChan
-        runProcess node $ do
-          taskQueue <- TaskQueue.start backend
-
-          spawnLocal $ forever $ do
-            (finalize, benchmarkScript, repo, commit) <- liftIO (readChan tasks)
-            spawnLocal $ do
-              result <- TaskQueue.execute taskQueue
-                THGenerated.stringDict
-                (THGenerated.benchmarkClosure benchmarkScript repo commit)
-              liftIO (finalize result)
-
-          let
-            onNewCommit :: (String -> IO ()) -> String -> Repo -> SHA -> IO ()
-            onNewCommit finalize benchmarkScript repo commit =
-              writeChan tasks (finalize, benchmarkScript, repo, commit)
-
-            paths :: Master.Paths
-            paths =
-              Master.Paths configFile rsyncPath gipeda
-
-          liftIO $ if oneShot
-            then Master.checkForNewCommits paths Master.OneShot onNewCommit
-            else Master.checkForNewCommits paths (Master.PeriodicRefresh (fromIntegral dt)) onNewCommit
+main = Logging.withStderrLogging $ do
+  p <- parser
+  join $ execParser $ info (helper <*> p) $
+    fullDesc
+      <> header "feed-gipeda - watch git repositories and feed benchmark results to gipeda"
+      <> progDesc "TODO"
