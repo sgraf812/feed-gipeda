de13c10e0c5ebf2c337b6138378403db7220c87f
Sebastian Graf <sgraf1337@gmail.com>
2016-07-13 23:35:40 +0200

    Shellified GitShell

--- src/FeedGipeda/GitShell.hs
+++ src/FeedGipeda/GitShell.hs
@@ -1,6 +1,7 @@
 {-| Functions for shelling out to @git@ to work with git repositories.
 -}
 
+{-# LANGUAGE OverloadedStrings #-}
 
 module FeedGipeda.GitShell
   ( isRepositoryRoot
@@ -17,13 +18,15 @@ module FeedGipeda.GitShell
 import           Data.Char        (isSpace)
 import           Data.Functor
 import           Data.Maybe       (listToMaybe)
+import           Data.Monoid      ((<>))
 import           Data.Set         (Set)
 import qualified Data.Set         as Set
+import qualified Data.Text        as Text
 import           FeedGipeda.Repo  (Repo)
 import qualified FeedGipeda.Repo  as Repo
+import           Shelly           (Sh, shelly)
+import qualified Shelly
 import           System.Directory (createDirectoryIfMissing)
-import           System.Exit      (ExitCode (..))
-import           System.Process   (callProcess, readProcessWithExitCode)
 
 
 type SHA
@@ -31,30 +34,26 @@ type SHA
 
 
 isRepositoryRoot :: FilePath -> IO Bool
-isRepositoryRoot path = do
-  (_, stdout, _)  <- readProcessWithExitCode
-    "git" ["-C", path, "rev-parse", "--git-dir"] ""
+isRepositoryRoot path = shelly $ do
+  stdout  <- Shelly.run "git" ["-C", Text.pack path, "rev-parse", "--git-dir"]
   -- testing for ".git" and "." (bare repo) should be good enough.
-  (return . maybe False (`elem` [".git", "."]) . listToMaybe . lines) stdout
+  (return . maybe False (`elem` [".git", "."]) . listToMaybe . lines) (Text.unpack stdout)
 
 
 mirror :: Repo -> FilePath -> IO ()
-mirror repo path = do
-  (_, _, _)  <- readProcessWithExitCode
-    "git" ["clone", "--mirror", "--quiet", Repo.uri repo, path] ""
-  return ()
+mirror repo path = shelly $
+  Shelly.run_ "git" ["clone", "--mirror", "--quiet", Text.pack (Repo.uri repo), Text.pack path]
 
 
 remoteRepo :: FilePath -> IO Repo
-remoteRepo path = do
-  (_, stdout, _)  <- readProcessWithExitCode
-    "git" ["-C", path, "ls-remote", "--get-url", "origin"] ""
-  return (Repo.unsafeFromString (init stdout)) -- strip the \n with init
+remoteRepo path = shelly $ do
+  stdout  <- Shelly.run "git" ["-C", Text.pack path, "ls-remote", "--get-url", "origin"]
+  return (Repo.unsafeFromString (init (Text.unpack stdout))) -- strip the \n with init
 
 
 fetch :: FilePath -> IO ()
-fetch path =
-  callProcess "git" ["-C", path, "fetch", "--quiet"]
+fetch path = shelly $
+  Shelly.run_ "git" ["-C", Text.pack path, "fetch", "--quiet"]
 
 
 {-| @sync repo@ tries to fetch updates from the remote @repo@ or creates a
@@ -82,18 +81,18 @@ firstCommit path =
 
 
 showHead :: FilePath -> FilePath -> IO (Maybe String)
-showHead path file = do
-  let
-    allArgs = ["-C", path, "show", "HEAD:" ++ file]
-  (exitCode, stdout, stderr) <- readProcessWithExitCode "git" allArgs ""
-  case exitCode of
-    ExitSuccess -> return (Just stdout)
-    ExitFailure _ -> return Nothing
+showHead path file = shelly $ do
+  stdout <- Shelly.errExit False $
+    Shelly.run "git" ["-C", Text.pack path, "show", "HEAD:" <> Text.pack file]
+  exitCode <- Shelly.lastExitCode
+  if exitCode == 0
+    then return (Just (Text.unpack stdout))
+    else return Nothing
 
 
 gitLogImpl :: FilePath -> [String] -> IO [SHA]
-gitLogImpl path additionalArgs = do
+gitLogImpl path additionalArgs = shelly $ do
   let
     allArgs = ["-C", path, "log", "--format=format:%H"] ++ additionalArgs
-  (_, stdout, _) <- readProcessWithExitCode "git" allArgs ""
-  return (filter (not . null) (lines stdout))
+  stdout <- Shelly.run "git" (map Text.pack allArgs)
+  return . filter (not . null) . lines . Text.unpack $ stdout
